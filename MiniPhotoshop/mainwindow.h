4bf654fe94c01c2029eca0fdd7ca5b45c294b762
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2024-04-14 00:53:02 +0200

    fixed library and added rotation

--- MiniPhotoshop/mainwindow.h
+++ MiniPhotoshop/mainwindow.h
@@ -66,6 +66,14 @@ private slots:
 
     void on_loadImage_clicked();
 
+    void on_rotateBtn_clicked();
+
+    void on_rotate90_clicked();
+
+    void on_rotate180_clicked();
+
+    void on_rotate270_clicked();
+
 private:
     Ui::MainWindow *ui;
 
